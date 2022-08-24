2a78ea8c35578ea9624020e286b68a28cf60093a
TaR3011 <95168051+TaR3011@users.noreply.github.com>
2022-08-24 19:50:47 +0300

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/Object Oriented Programming (OOP)/01-Challenges/script.js
@@ -0,0 +1,26 @@
+const Car = function (make, speed) {
+  this.speed = speed;
+  this.make = make;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.break = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const bmw = new Car("BMW", 120);
+const mercedes = new Car("Merceds", 95);
+
+bmw.accelerate();
+bmw.break();
+
+mercedes.accelerate();
+mercedes.break();
+mercedes.accelerate();
+bmw.break();
+bmw.accelerate();
