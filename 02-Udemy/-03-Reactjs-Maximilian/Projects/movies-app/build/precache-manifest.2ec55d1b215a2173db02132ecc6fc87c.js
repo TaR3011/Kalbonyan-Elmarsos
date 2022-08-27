86188e68f73b4488c24a08cd8bb651b9f194eca2
TaR3011 <95168051+TaR3011@users.noreply.github.com>
2022-08-27 17:49:06 +0300

    Add files via upload

---
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/movies-app/build/precache-manifest.2ec55d1b215a2173db02132ecc6fc87c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d28518924edb728aedc2ed187a6a0e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42faba1aaf391d405a04",
+    "url": "/static/css/main.b9b815ba.chunk.css"
+  },
+  {
+    "revision": "22daeca5b02b55968147",
+    "url": "/static/js/2.42d1fe8e.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.42d1fe8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42faba1aaf391d405a04",
+    "url": "/static/js/main.69ff6004.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.69ff6004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4054bc057732f69f878d",
+    "url": "/static/js/runtime-main.13ba5424.js"
+  }
+]);
