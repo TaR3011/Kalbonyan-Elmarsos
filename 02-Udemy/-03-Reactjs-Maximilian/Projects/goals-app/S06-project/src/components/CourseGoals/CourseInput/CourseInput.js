1b583adaee5454e62c76584a4c697949a72a4d0d
TaR3011 <95168051+TaR3011@users.noreply.github.com>
2022-09-22 14:32:19 +0300

    Add files via upload

---
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/goals-app/S06-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+// import styled from "styled-components";
+
+import Button from "../../UI/Button/Button";
+import styles from "./CourseInput.module.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isVaild, setIsVaild] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsVaild(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsVaild(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div
+        className={`${styles["form-control"]} ${!isVaild && styles.invalid}`}
+      >
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
