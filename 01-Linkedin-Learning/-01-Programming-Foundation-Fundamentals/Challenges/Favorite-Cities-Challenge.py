58b8dec8d88a7ebc99f027377066af6429c61617
TaR3011 <95168051+TaR3011@users.noreply.github.com>
2022-09-22 16:42:32 +0300

    Update Favorite-Cities-Challenge.py

--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Favorite-Cities-Challenge.py
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Favorite-Cities-Challenge.py
@@ -1,1 +1,8 @@
 
+def favorite_city(cityName):
+  print("one of my favorite cities is " , cityName)
+
+
+favorite_city("Makkah , Saudi Arbia")
+favorite_city("Madina , Saudi Arbia")
+favorite_city("Riyadh , Saudi Arbia")
